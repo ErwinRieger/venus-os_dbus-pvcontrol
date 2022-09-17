0a8d13f95e760a0594b9d91a6c0323174bdad11c
ErwinRieger <erwin.rieger@ibrieger.de>
2022-09-17 13:50:11 +0200

    Added exit_on_error() and sys.exit() when rs6/mp2 service not found on dbus.

--- dbus-pvcontrol.py
+++ dbus-pvcontrol.py
@@ -3,6 +3,9 @@
 """
 * Raspi muss durch inverter oder batterie versorgt werden, damit das system
   bereit ist um grosse leistung beim einschalten der hausversorgung zu uebernehmen.
+
+Nice to have: set MaxPRs only if mp2 is inverting.
+
 """
 from gi.repository import GLib
 import platform
@@ -13,6 +16,7 @@ import os, time
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), './ext/velib_python'))
 from vedbus import VeDbusService
 from dbusmonitor import DbusMonitor
+from ve_utils import exit_on_error
 
 onPower =   2750 # watts of rs6000 power when we turn on slave the multiplus, depends on ac current limit of multiplus
 OnTimeout = 1800
@@ -33,6 +37,26 @@ class PVControl(object):
                 }
 
         self._dbusmonitor = DbusMonitor(dbus_tree, valueChangedCallback=self._value_changed)
+
+	# Get dynamic servicename for rs6 (ve.can)
+        # self.vecan_service = self.waitForService('com.victronenergy.inverter')
+        serviceList = self._get_service_having_lowest_instance('com.victronenergy.inverter')
+        if not serviceList:
+            # Restart process
+            logging.info("service com.victronenergy.inverter not registered yet, exiting...")
+            sys.exit(0)
+        self.vecan_service = serviceList[0]
+        logging.info("service of inverter rs6: " +  self.vecan_service)
+
+	# Get dynamic servicename for mp2 (ve.bus)
+        # self.vebus_service = self.waitForService('com.victronenergy.vebus')
+        serviceList = self._get_service_having_lowest_instance('com.victronenergy.vebus')
+        if not serviceList:
+            # Restart process
+            logging.info("service com.victronenergy.vebus not registered yet, exiting...")
+            sys.exit(0)
+        self.vebus_service = serviceList[0]
+        logging.info("service of mp2: " + self.vebus_service)
 
         self._dbusservice = VeDbusService(servicename)
 
@@ -61,14 +85,6 @@ class PVControl(object):
         self._dbusservice['/A/MaxPRs'] = 0
         self._dbusservice['/A/MaxPon'] = 0
 
-	# Get dynamic servicename for rs6 (ve.can)
-        self.vecan_service = self.waitForService('com.victronenergy.inverter')
-        logging.info("service of inverter rs6: " +  self.vecan_service)
-
-	# Get dynamic servicename for mp2 (ve.bus)
-        self.vebus_service = self.waitForService('com.victronenergy.vebus')
-        logging.info("service of mp2: " + self.vebus_service)
-
         # read initial value of rs6000 output power
         self.watt = self._dbusmonitor.get_value(self.vecan_service, "/Ac/Out/L1/P")
         logging.info('initial rs6 watts: %d' % self.watt)
@@ -82,7 +98,8 @@ class PVControl(object):
         self.MaxPMp = 0
         self.MaxPRs = 0
 
-        GLib.timeout_add(10000, self._update)
+        # GLib.timeout_add(10000, self._update)
+        GLib.timeout_add(10000, exit_on_error, self._update)
 
     def _update(self):
 
@@ -130,6 +147,8 @@ class PVControl(object):
                         self._dbusservice["/A/MaxPon"] = self.watt
                 self.endTimer = time.time() + OnTimeout
 
+    # Does not work, seems that _dbusmonitor does not re-scan the bus...
+    """
     # Wait for and get dynamic servicename for rs6/mp2
     def waitForService(self, sn):
         serviceList = self._get_service_having_lowest_instance(sn)
@@ -141,6 +160,7 @@ class PVControl(object):
         service = serviceList[0]
         logging.info("service for %s: %s" % (sn, service))
         return service
+    """
 
     # returns a tuple (servicename, instance)
     def _get_service_having_lowest_instance(self, classfilter=None): 
