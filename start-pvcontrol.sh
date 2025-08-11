b4c4acde9133d2292cab2564b78372d518fce4a2
ErwinRieger <erwin.rieger@ibrieger.de>
2025-08-11 18:06:36 +0200

    Install using SetupHelper.

--- start-pvcontrol.sh
+++ start-pvcontrol.sh
@@ -3,5 +3,5 @@
 
 . /opt/victronenergy/serial-starter/run-service.sh
 
-app="python3 /data/etc/venus-os_dbus-pvcontrol/dbus-pvcontrol.py"
+app="python3 /data/venus-os_dbus-pvcontrol/dbus-pvcontrol.py"
 start 
